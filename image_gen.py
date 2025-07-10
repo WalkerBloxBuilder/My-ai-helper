406908da306b4fed3f0a103e20cb4ce5c9dd39ed
WalkerBloxBuilder <Walker.yeargan@gmail.com>
2025-07-09 17:21:41 -0700

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
 def generate_image(prompt):
     response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
