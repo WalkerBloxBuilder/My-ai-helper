0ab1dc17154bd28ac77419036a70cae69fa89e7d
WalkerBloxBuilder <Walker.yeargan@gmail.com>
2025-07-09 17:17:41 -0700

    image_gen.py

---
+++ image_gen.py
@@ -0,0 +1,8 @@
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_image(prompt):
+    response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
+    return response['data'][0]['url']
